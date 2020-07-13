65f511183aa64767df115eb9bf19d99f555e8187
ianagbip1oti <ianagbip1oti@gmail.com>
2020-07-13 23:45:45 +0000

    👌 IMPROVE: Better build/ci setups

--- setup.py
+++ setup.py
@@ -1,10 +1,5 @@
 from setuptools import find_packages, setup
 
-fmt_deps = ["autoflake==1.3.1", "isort==4.3.21", "black==19.3b0"]
-setup_deps = ["setuptools-scm==3.3.3"]
-test_deps = ["pytest==5.2.1", "pytest-cov==2.8.1", "behave==1.2.6"]
-extras = {"fmt": fmt_deps, "test": test_deps}
-
 
 def install_requires():
     with open("requirements.txt") as f:
@@ -16,9 +11,7 @@ setup(
     packages=find_packages(),
     use_scm_version=True,
     install_requires=install_requires(),
-    tests_require=test_deps,
-    extras_require=extras,
-    setup_requires=setup_deps,
+    setup_requires=["setuptools-scm==3.3.3"],
     entry_points={
         "console_scripts": [
             "tsktsk=tsktsk.__main__:main",
