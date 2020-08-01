54017fef94a6042e6d2b7e21f84d59bd9bcc96a8
ianagbip1oti <ianagbip1oti@gmail.com>
2020-08-01 21:11:31 +0000

    📦 NEW: Allow mapping of discord channels to github repos

--- tsktsk/config.py
+++ tsktsk/config.py
@@ -13,6 +13,10 @@ def split_tuple_list(var: str) -> Iterable[Tuple[str]]:
     return (tuple(s.split(":")) for s in os.environ.get(var, "").split(","))
 
 
+def dict_from_env(var: str) -> Dict[str, str]:
+    return {d[0]: d[1] for d in split_tuple_list(var)}
+
+
 class Config:
     @property
     def single_github_repository(self) -> Optional[str]:
@@ -36,10 +40,18 @@ class Config:
 
     @property
     def discord_users(self) -> Dict[str, str]:
-        return {u[0]: u[1] for u in split_tuple_list("TSKTSK_DISCORD_USERS")}
+        return dict_from_env("TSKTSK_DISCORD_USERS")
+
+    @property
+    def discord_channels(self) -> Dict[str, str]:
+        return dict_from_env("TSKTSK_DISCORD_CHANNELS")
 
     @property
     def github_users(self) -> Dict[str, GithubAuth]:
         return {
             u[0]: GithubAuth(*u[1:]) for u in split_tuple_list("TSKTSK_GITHUB_USERS")
         }
+
+    @property
+    def github_repositories(self) -> Dict[str, str]:
+        return dict_from_env("TSKTSK_GITHUB_REPOS")
