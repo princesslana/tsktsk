bb802c6a987c86bbb873076f68dcad52050cfa5f
Ayman Izzeldin <ayman.izzeldin@gmail.com>
2020-08-06 00:58:25 +0200

    🐛 FIX: add missing github issues done status parsing

--- tsktsk/repository/github.py
+++ tsktsk/repository/github.py
@@ -1,5 +1,6 @@
 import contextlib
 import dataclasses
+from datetime import datetime
 from typing import Any, Dict, Iterator, Optional
 
 import requests
@@ -44,6 +45,13 @@ def task_from_json(issue: JsonObject) -> GithubTask:
     value = next((k for k, v in VALUE.items() if v in labels), VALUE_DEFAULT)
     effort = next((k for k, v in EFFORT.items() if v in labels), EFFORT_DEFAULT)
 
+    closed_at = issue["closed_at"]
+    done = (
+        datetime.strptime(closed_at, "%Y-%m-%dT%H:%M:%SZ").strftime("%Y%m%d")
+        if closed_at
+        else None
+    )
+
     task = GithubTask(
         key=issue["number"],
         message=message,
@@ -53,6 +61,7 @@ def task_from_json(issue: JsonObject) -> GithubTask:
         additional_labels=sorted(
             l for l in labels if l not in VALUE.values() and l not in EFFORT.values()
         ),
+        done=done,
     )
     return task
 
